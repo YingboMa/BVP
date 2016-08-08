e7472cac696814788bac77f3b25d2f12dc67fb43
YingboMa <mayingbo5@gmail.com>
2016-08-08 13:11:01 -0400

    Correct version

--- General_nonlinear.jl
+++ General_nonlinear.jl
@@ -5,30 +5,20 @@ using ForwardDiff
 function BVP_nonlinear(f::Function, a::Number,  b::Number,
                        a₀::Number,  b₀::Number, c₀::Number,
                        a₁::Number,  b₁::Number, c₁::Number;
-                       N=Int((b-a)*20),         y=0,
-                       tol = 1e-3,  maxIt=1e4)
-    # if y==0
-    #     y=randn(N+1)
-    # end
-    # N = 50
-    # a = 0.0
-    # b = 10.0
+                       N=Int((b-a)*15),         y=0,
+                       tol = 1e-1,  maxIt=1e3)
     i = 1:N-1
-    x = linspace(a, b, N+1)
     h = (b-a) / N
-
-    # a₀ = 1.
-    # b₀ = 0.
-    # c₀ = 0.
-
-    # a₁ = 1.
-    # b₁ = 0.
-    # c₁ = 1.
+    x = [a:h:b;]
+    if y==0
+        slope = (c₁/a₁ - c₀/a₀) / (b-a)
+        y = convert(Array{Float64}, slope*x+c₀/a₀)
+    end
 
     A = Array(Float64, N+1)
     B = Array(Float64, N+1)
     C = Array(Float64, N+1)
-    F = Array(Float64, N+1)
+    F = zeros(Float64, N+1)
 
     h² = 1 / h^2
     h₂ = h*2
@@ -56,6 +46,7 @@ function BVP_nonlinear(f::Function, a::Number,  b::Number,
     y_n = y
     y_n1 = similar(y_n)
     updateF!(F, f, x, y_n, N, h)
+    JG = 1.
     k = 0
 
     while norm(JG) >= tol
@@ -63,14 +54,14 @@ function BVP_nonlinear(f::Function, a::Number,  b::Number,
         G = M*y_n - F
         JG = J \ -G
         y_n1 = JG + y_n
+        @assert all(isfinite(y_n1))
         updateF!(F, f, x, y_n1, N, h)
         y_n = y_n1
             k+=1
         if k > maxIt
-            println("Max iterations reached")
+            warn("Max iterations reached")
             break
         end
-    end
     return y_n1
 end
 
@@ -78,24 +69,24 @@ function myBessel(x, y, yp)
     - ( x*yp - (x^2-α^2)*y ) / x^2
 end
 
-function updateF!(F::Vector, f::Function, x::LinSpace, y::Vector, N::Int, h::Number)
+function foo(x,y,yp)
+    sin(yp)
+end
+
+function updateF!(F::Vector, f::Function, x::Vector, y::Vector, N::Int, h::Number)
     for i = 2:N
         F[i] = f(x[i], y[i], (y[i+1] - y[i-1])/(2h))
     end
     return F
 end
 
-function compJ(f::Function, x::LinSpace, y::Vector, h::Number, N::Number, M::Tridiagonal)
+function compJ(f::Function, x::Vector, y::Vector, h::Number, N::Number, M::Tridiagonal)
     J = Tridiagonal(zeros(N - 1), zeros(N), zeros(N - 1))
     ff(xys) = f(xys[1],xys[2],(xys[3]-xys[4])/(2h))
-    ff_1_N(xys) = f(xys[1],xys[2],(xys[3]-xys[2])/h)
-    g = ForwardDiff.gradient(ff_1_N)([x[1],y[1],y[2]])
-    J.d[1]  = M.d[1] - g[2]
-    #J.dl[1] = g[3]
-    J.du[1] = M.du[1] - g[3]
-    g = ForwardDiff.gradient(ff_1_N)([x[N],y[N],y[N-1]])
-    J.d[N]  = M.d[N] - g[2]
-    J.dl[N-1] = M.dl[N-1] - g[3]
+    J.d[1]  = M.d[1]
+    J.du[1] = M.du[1]
+    J.d[N]  = M.d[N]
+    J.dl[N-1] = M.dl[N-1]
     for i = 2:N-1
         g = ForwardDiff.gradient(ff)([x[i],y[i],y[i+1],y[i-1]])
         J.d[i] = M.d[i] - g[2]
